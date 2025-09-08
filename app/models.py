4a6d92ea5bac8fe1af03205952e925ca8eb89820
Atharva Parkhe <patharv777@gmail.com>
2025-09-09 00:48:27 +0530

    img

--- app/models.py
+++ app/models.py
@@ -4,6 +4,6 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField(blank=True)
-
+    img = models.ImageField(upload_to="product", height_field=None, width_field=None, max_length=None)
     def __str__(self):
         return self.name
