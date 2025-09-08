b83206dc1967bccbcfc5e40c54eb95010cb9e5d9
Atharva Parkhe <patharv777@gmail.com>
2025-09-08 13:02:51 +0530

    something

--- core/settings.py
+++ core/settings.py
@@ -1,11 +1,12 @@
 from decouple import config
 from pathlib import Path
-import os
+# import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config("SECRET_KEY")
 
+# DEBUG = True
 DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
@@ -18,6 +19,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'storages',
     "app"
 ]
 
@@ -33,11 +35,13 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'core.urls'
 
+
+TEMPLATES_DIRS = []
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [],
-        'APP_DIRS': True,
+        'APP_DIRS': TEMPLATES_DIRS,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.request',
@@ -50,12 +54,24 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'core.wsgi.application'
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.contrib.gis.db.backends.postgis',
+#         'NAME': config("DB_NAME"),
+#         'USER': config("DB_USER"),
+#         'PASSWORD': config("DB_PASSWORD"),
+#         'HOST': config("DB_HOST"),
+#         'PORT': config("DB_PORT"),
+#     }
+# }
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -72,7 +88,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-# Language and Time 
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'Asia/Kolkata'
 DATETIME_FORMAT = '%d/%m/%Y %H:%M:%S' 
@@ -80,10 +95,29 @@ USE_I18N = False
 USE_L10N = False
 USE_TZ = False
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join("static")
 
-MEDIA_URL = '/data/'
-MEDIA_ROOT = os.path.join("data")
+# STATIC_URL = 'static/'
+# STATIC_ROOT = os.path.join("static")
+
+# MEDIA_URL = '/data/'
+# MEDIA_ROOT = os.path.join("data")
+
+# # AWS Config
+AWS_ACCESS_KEY_ID = config("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = config("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = config("AWS_STORAGE_BUCKET_NAME")
+AWS_S3_REGION_NAME = "ap-south-1"
+AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+# AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+AWS_DEFAULT_ACL = None
+
+STATIC_LOCATION = 'static'
+STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/"
+STATICFILES_STORAGE = 'core.storages.StaticStore'
+
+PUBLIC_MEDIA_LOCATION = 'media'
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
+DEFAULT_FILE_STORAGE = 'core.storages.MediaStore'
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- dockerfile
+++ dockerfile
@@ -1,24 +1,50 @@
+# FROM python:3.11-slim
+
+# ENV PYTHONDONTWRITEBYTECODE=1
+# ENV PYTHONUNBUFFERED=1
+
+# WORKDIR /app
+
+# RUN apt-get update \
+#     && apt-get install -y --no-install-recommends \
+#         postgresql-client \
+#         build-essential \
+#         libpq-dev \
+#     && rm -rf /var/lib/apt/lists/*
+
+# COPY requirements.txt .
+# RUN pip install --no-cache-dir -r requirements.txt
+
+# COPY . .
+
+# RUN mkdir -p /app/static /app/data
+
+# EXPOSE 8000
+
+# CMD ["python", "manage.py", "runserver","0.0.0.0:8000"]
+
+
+# Use official Python slim image
 FROM python:3.11-slim
 
-ENV PYTHONDONTWRITEBYTECODE=1
-ENV PYTHONUNBUFFERED=1
-
+# Set working directory
 WORKDIR /app
 
-RUN apt-get update \
-    && apt-get install -y --no-install-recommends \
-        postgresql-client \
-        build-essential \
-        libpq-dev \
+# Install system dependencies (for psycopg2 & pillow if needed)
+RUN apt-get update && apt-get install -y \
+    build-essential \
+    libpq-dev \
     && rm -rf /var/lib/apt/lists/*
 
+# Install Python dependencies
 COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 
+# Copy project files
 COPY . .
 
-RUN mkdir -p /app/static /app/data
-
+# Expose app port
 EXPOSE 8000
 
-CMD ["python", "manage.py", "runserver","0.0.0.0:8000"]
+# Run migrations + start server
+CMD ["gunicorn", "project_root.wsgi:application", "--bind", "0.0.0.0:8000"]
